f45d9a6fa4003fb52a5df48b9fdfe7f1deb76e86
hinary <shpak1352@gmail.com>
2023-09-26 21:33:26 +0300

    "data" layer prepared

--- data/db.go
+++ data/db.go
@@ -1,6 +1,10 @@
 package data
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
 
 type DBConfig struct {
 	Path string
@@ -9,4 +13,27 @@ type DBConfig struct {
 type Dictionary struct {
 	db    *gorm.DB
 	Words *WordsDictionary
+}
+
+func (d *Dictionary) GetDB() *gorm.DB {
+	return d.db
+}
+
+func NewDictionary(config DBConfig) *Dictionary {
+	db, err := gorm.Open(sqlite.Open(config.Path), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Error),
+	})
+
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	db.AutoMigrate(&Word{})
+
+	dictionary := &Dictionary{
+		db: db,
+		Words: NewWordDictionary(db),
+	}
+
+	return dictionary
 }
--- data/words.go
+++ data/words.go
@@ -1,6 +1,10 @@
 package data
 
-import "gorm.io/gorm"
+import (
+	"fmt"
+
+	"gorm.io/gorm"
+)
 
 type WordProperties struct {
 	RussianWord string `json:"ru"`
@@ -16,3 +20,54 @@ type WordsDictionary struct {
 	db *gorm.DB
 }
 
+func NewWordDictionary(db *gorm.DB) *WordsDictionary {
+	return &WordsDictionary{db}
+}
+
+func (d *WordsDictionary) GetAll() ([]Word, error) {
+	words := make([]Word, 0)
+	err := d.db.Find(&words).Error
+
+	return words, err
+}
+
+func (d *WordsDictionary) GetOne(id int) (Word, error) {
+	word := Word{}
+	err := d.db.First(&word, id).Error
+
+	return word, err
+}
+
+func (d *WordsDictionary) Delete(id int) (error) {
+	err := d.db.Delete(&Word{}, id).Error
+
+	return err
+}
+
+func (d *WordsDictionary) Update(id int, upd WordProperties) (error) {
+	w := Word{}
+	err := d.db.Find(&w, id).Error
+
+	if err != nil {
+		return err
+	}
+
+	if w.ID == 0 {
+		return fmt.Errorf("a word with id %d not found", id)
+	}
+
+	w.WordProperties = upd
+
+	err = d.db.Save(&w).Error
+
+	return err
+}
+
+func (d *WordsDictionary) Add(upd WordProperties) (error) {
+	word := Word{
+		WordProperties: upd,
+	}
+	err := d.db.Create(&word).Error
+
+	return err
+}
